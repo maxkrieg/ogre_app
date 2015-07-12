74c31aba2cd115f52790f14a1fff79ba55e35ed7
maxkrieg <maxwell.krieg@gmail.com>
2015-07-12 00:39:45 -0400

    fixing login field values

--- app/scripts/controllers/loginController.js
+++ app/scripts/controllers/loginController.js
@@ -4,35 +4,22 @@
 
   var LoginController = function(appSettings, loginFactory) {
     this.appSettings = appSettings;
-
-    this.loginForm.getEmail = function(emailInput) {
-      if (emailInput.$valid) {
-        return emailInput;
-      } else {
-        alert('email invalid');
-      }
-    };
-
-    this.loginForm.getPassword = function(passwordInput) {
-      if (passwordInput.$valid) {
-        return passwordInput;
-      } else {
-        alert('password invalid');
-      }
-    };
+    this.loginForm = {};
+    this.loginForm.email = "";
+    this.loginForm.password = "";
 
 
-    this.authenticateUser = function() {
-      loginFactory.authenticateUser()
-        .success(function(data, status) {
-          localStorage.setItem('token', data.token);
-          localStorage.getItem('token');
-        })
-        .error(function(data, status, headers, config) {
-          console.log("Error logging in");
-          alert("Error logging in");
-        });
-    };
+    // this.authenticateUser = function() {
+    //   loginFactory.authenticateUser()
+    //     .success(function(data, status) {
+    //       localStorage.setItem('token', data.token);
+    //       localStorage.getItem('token');
+    //     })
+    //     .error(function(data, status, headers, config) {
+    //       console.log("Error logging in");
+    //       alert("Error logging in");
+    //     });
+    // };
 
   };
 
--- app/scripts/services/loginFactory.js
+++ app/scripts/services/loginFactory.js
@@ -5,23 +5,22 @@
   // Create a customers factory
   var loginFactory = function($http, appSettings) {
     var factory = {};
-    factory.appSettings = appSettings;
+    // factory.appSettings = appSettings;
 
-    factory.authenticateUser = function() {
-      return $http.post(this.appSettings.railsURI + '/login', {
-        credentials: {
-          email: $('#email').val(),
-          password: $('#password').val()
-        }
-      }, {
-        headers: undefined,
-        processData: false
-      });
-    };
+    // factory.authenticateUser = function() {
+    //   return $http.post(this.appSettings.railsURI + '/login', {
+    //     credentials: {
+    //       email: $('#email').val(),
+    //       password: $('#password').val()
+    //     }
+    //   }, {
+    //     headers: undefined,
+    //     processData: false
+    //   });
+    // };
 
     return factory;
   };
-
   loginFactory.$inject = ['$http', 'appSettings'];
 
   angular.module('ogreApp').factory('loginFactory', loginFactory);

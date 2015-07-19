75d4be0b09f1b1e0d5db25a1aaf1222bbf7673f5
maxkrieg <maxwell.krieg@gmail.com>
2015-07-19 15:01:49 -0400

    adds heroku url to values.js

--- app/scripts/services/values.js
+++ app/scripts/services/values.js
@@ -1,5 +1,5 @@
 angular.module("ogreApp").value('appSettings', {
   title: "Ogre Buddies Application",
   version: "1.0",
-  railsURI: "http://localhost:3000"
+  railsURI: "https://ogre-buddies.herokuapp.com" || "http://localhost:3000"
 });
